560514852b067a7bcc955bc89446d0842ef7fc7f
Randy Brunecz <rkbrunecz@gmail.com>
2015-08-31 02:58:13 -0400

    Added game states and a game loop to main.cpp

--- JRpg/Code/main.cpp
+++ JRpg/Code/main.cpp
@@ -1,4 +1,19 @@
+//CONSTANTS
+const enum Game_States //Dictates the state the game is in
+{
+	Play,
+	Pause,
+	Quit,
+	MainMenu
+} state;
+
 int main()
 {
+	//GAME LOOP
+	while (state != Quit)
+	{
+
+	}
+
 	return 0;
 }
