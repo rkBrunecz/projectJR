1c3b6051152d5eecf6f46b07bc3d56aa06b84775
Randy Brunecz <rkbrunecz@gmail.com>
2015-09-03 03:25:04 -0400

    Documented the code

--- Code/SpecialEffect.cpp
+++ Code/SpecialEffect.cpp
@@ -1,12 +1,22 @@
 #include "SpecialEffect.h"
 #include "Graphic.h"
 
+/*
+fadeOut
+Parameters:
+	window: The game window
+	graphics[]: The array that holds all of the graphics
+	arraySize: Size of the graphics array
 
+This method performs a fade out effect.
+*/
 void SpecialEffect::fadeOut(sf::RenderWindow* window, Graphic* graphics[], int arraySize)
 {
+	//LOCAL VARIABLES
 	sf::Clock clock;
 	int alpha = 255;
 
+	//Perform a fade out over a half second
 	while (clock.getElapsedTime() <= sf::seconds(0.5f))
 	{
 		for (int i = 0; i < arraySize; i++)
@@ -16,11 +26,22 @@ void SpecialEffect::fadeOut(sf::RenderWindow* window, Graphic* graphics[], int a
 	}
 }
 
+/*
+fadeIn
+Parameters:
+window: The game window
+graphics[]: The array that holds all of the graphics
+arraySize: Size of the graphics array
+
+This method performs a fade in effect.
+*/
 void SpecialEffect::fadeIn(sf::RenderWindow* window, Graphic* graphics[], int arraySize)
 {
+	//LOCAL VARIABLES
 	sf::Clock clock;
 	int alpha = 0;
 
+	//Perform a fade in over a half second
 	while (clock.getElapsedTime() <= sf::seconds(0.5f))
 	{
 		for (int i = 0; i < arraySize; i++)
