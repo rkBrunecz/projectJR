e162f9bdb01c25a11b757330ca50aa9c9ed167b6
Randy Brunecz <rkbrunecz@gmail.com>
2015-09-06 13:34:44 -0400

    Fixed a bug with how the map tiled.

--- Code/Map.cpp
+++ Code/Map.cpp
@@ -123,8 +123,8 @@ void Map::draw(sf::RenderWindow* window)
 			//Set the part of the tile map to draw to the window
 			tiles.setTextureRect(sf::IntRect((map[i][j].column * tileSize) - tileSize, 
 											 (map[i][j].row * tileSize) - tileSize, 
-											  map[i][j].column * tileSize, 
-											  map[i][j].row * tileSize)); 
+											  tileSize, 
+											  tileSize)); 
 			
 			window->draw(tiles); //Draw the tile
 		}			
