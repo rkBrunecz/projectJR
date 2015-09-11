8e04e7fc10b6d13976eac3521ef322377a596b54
Randy Brunecz <rkbrunecz@gmail.com>
2015-09-11 19:45:43 -0400

    Changed the camera class to a wrapper of SFML view

--- Code/Camera.cpp
+++ Code/Camera.cpp
@@ -1,82 +1,12 @@
 #include "Camera.h"
-#include <SFML\Graphics.hpp>
-#include <iostream>
 
-void Camera::updateCameraPosition(float velocityX, float velocityY, int playerX, int playerY)
+void Camera::setBounds(int x, int y)
 {
-	if (playerX > displayWidth / 2 && playerX < mapX - (displayWidth / 2))
-		x += velocityX;
-
-	if (playerY > displayHeight / 2 && playerY < mapY - (displayHeight / 2))
-		y += velocityY;
+	cameraBoundX = x;
+	cameraBoundY = y;
 }
 
-void Camera::setDisplay(int displayW, int displayH)
+sf::Vector2f Camera::getCameraBounds()
 {
-	displayWidth = displayW;
-	displayHeight = displayH;
-}
-
-void Camera::setCameraMaxBounds(int x, int y)
-{
-	mapX = x;
-	mapY = y;
-}
-
-int Camera::getXPos()
-{
-	return x;
-}
-
-int Camera::getYPos()
-{
-	return y;
-}
-
-int Camera::getUpperBoundWidth(int tileSize)
-{
-	if ((x + displayWidth) / tileSize > (mapX / tileSize))
-		return mapX / tileSize;
-	else
-		return (x + displayWidth) / tileSize;
-}
-
-int Camera::getUpperBoundHeight(int tileSize)
-{
-	if ((y + displayHeight) / tileSize > mapY / tileSize)
-		return mapY / tileSize;
-	else
-		return (y + displayHeight) / tileSize;
-}
-
-int Camera::getLowerBoundWidth(int tileSize)
-{
-	if ((x - displayWidth) / tileSize < 0)
-		return 0;
-	else
-		return (x - displayWidth) / tileSize;
-}
-
-int Camera::getLowerBoundHeight(int tileSize)
-{
-	if ((y - displayHeight) / tileSize < 0)
-		return 0;
-	else
-		return (y - displayHeight) / tileSize;
-}
-
-bool Camera::pastCameraBoundaryX(int x)
-{
-	if (x < displayWidth / 2 || x > mapX - (displayWidth / 2))
-		return true;
-
-	return false;
-}
-
-bool Camera::pastCameraBoundaryY(int y)
-{
-	if (y < displayHeight / 2 || y > mapY - (displayHeight / 2))
-		return true;
-
-	return false;
+	return sf::Vector2f(cameraBoundX - (getSize().x * 0.5), cameraBoundY - (getSize().y * 0.5));
 }
--- Code/Camera.h
+++ Code/Camera.h
@@ -1,26 +1,16 @@
 #ifndef Camera_H
 #define Camera_H
 
-class Camera
+#include <SFML\Graphics.hpp>
+
+class Camera : public sf::View
 {
 public:
-	void updateCameraPosition(float velocityX, float velocityY, int playerX, int playerY);
-	void setDisplay(int displayW, int displayH);
-	void setCameraMaxBounds(int x, int y);
-
-	int getXPos();
-	int getYPos();
-	int getUpperBoundWidth(int tileSize);
-	int getUpperBoundHeight(int tileSize);
-	int getLowerBoundWidth(int tileSize);
-	int getLowerBoundHeight(int tileSize);
-
-	bool pastCameraBoundaryX(int x);
-	bool pastCameraBoundaryY(int y);
+	void setBounds(int x, int y);
+	sf::Vector2f getCameraBounds();
 
 private:
-	int x = 0, y = 0;
-	int displayWidth = 0, displayHeight = 0, mapX, mapY;
+	int cameraBoundX, cameraBoundY;
 };
 
 #endif
