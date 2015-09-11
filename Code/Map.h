f35a7cbdf973f370b473746011b3393b9b418302
Randy Brunecz <rkbrunecz@gmail.com>
2015-09-11 19:48:38 -0400

    Made huge optimizations to the map code; CPU usage around 0-1%

--- Code/Map.h
+++ Code/Map.h
@@ -9,26 +9,26 @@ This class handles creates and draws a map. It also performs other map related u
 #ifndef Map_H
 #define Map_H
 
-#include <SFML\Graphics.hpp>
 #include "Graphic.h"
+#include <SFML\Graphics.hpp>
 
 class Map : public Graphic
 {
 public:
 	//PUBLIC FUNCTIONS
-	Map();
+	Map(Camera* camera);
 	~Map();
-	std::string toString();
 	void draw(sf::RenderWindow* window);
 	void setColor(int r, int g, int b, int a);
 
 private:
 	//PRIAVTE FUNCTIONS
-	void initialize(std::ifstream& mapFile);
+	void initialize(std::ifstream& mapFile, Camera* camera);
 	void populateArray(std::ifstream& mapFile);
+	void drawMap();
 
 	//PRIVATE CONSTANTS
-	int tileSize = 32; //This is the width and height of each of the tiles
+	int TILE_SIZE = 32; //This is the width and height of each of the tiles
 
 	//PRIVATE VARIABLES
 	struct Tile //Contains information about a tile such as the row and column it is found in the tileSheet, and special transformation information.
@@ -36,9 +36,9 @@ private:
 		unsigned short row, column, transformation;
 	};
 	Tile** map; //This is the 2d array that contains the all of the tile information needed for the map
-	sf::Sprite tiles;
+	sf::Sprite tiles, mapSprite;
 	sf::Texture tileSheet;
-	sf::Clock clock;
+	sf::RenderTexture mapTexture;
 	int numRows, numColumns; //numRows and numColumns contain the total number of rows and columns in the array
 };
 
